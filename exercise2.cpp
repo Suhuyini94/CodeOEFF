5a31fbde827955bc37fe2ade3126a0f87a60ef52
Suhuyini94 <suhuyini86@gmail.com>
2020-06-21 20:19:46 +0000

    /*This program prompts the user to input a number. It then output the number and a message saying whether the number ispositive, negative or zero*/

--- exercise2.cpp
+++ exercise2.cpp
@@ -5,23 +5,24 @@ positive, negative, or zero.*/
 #include <iostream>
 using namespace std;
 
+
 int main()
 {
     int x;
-    cout << "Enter a number!" << endl;
+    cout <<"Enter a number!" << endl;
     cin >> x;
     while (x == 0)
     {
-        cout << "The numeber you entered is zero" << endl;
+        cout << x <<" is zero" << endl;
         return 0;
     }
     if (x > 0)
     {
-        cout << "The number you entered is positive" << endl;
+        cout << x <<" is positive" << endl;
     }
     else
     {
-        cout << "The number you entered is negative" << endl;
+        cout << x <<" is negative" << endl;
     }
     return 0;
 }
